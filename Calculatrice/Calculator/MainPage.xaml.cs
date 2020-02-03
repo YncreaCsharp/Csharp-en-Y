8618d13197913c8d6260415c582133c9822403fc
Dochy <dimitri.dochy59@gmail.com>
2020-02-03 16:44:03 +0100

    Correctif de bug
     reste que le design et les virgules

--- Calculatrice/Calculator/MainPage.xaml.cs
+++ Calculatrice/Calculator/MainPage.xaml.cs
@@ -22,78 +22,148 @@ namespace Calculator
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Random rnd = new Random();
 
         Double resultValue = 0;
         String operationPerformed = "";
         bool isOperationPerformed = false;
+        bool isEqualPressed = false;
 
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e){
-            if ((Result.Text == "0") || (isOperationPerformed))
-                Result.Text = ""; // . Clear() ?????????
-
-            isOperationPerformed = false;
+        // For button 0-1-2-3-4-5-6-7-8-9
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             Button button = (Button)sender;
-            if (button.Content == ".")
-            {
-                if (!Result.Text.Contains("."))
-                    Result.Text = Result.Text + button.Content;
 
+            if (isEqualPressed)
+            {
+
+                //reset everything ( if we have a result and we press a new nummber, no operation)
+                resultValue = 0;
+                operationPerformed = "";
+                isOperationPerformed = false;
+                isEqualPressed = false;
+                Result.Text = button.Content.ToString();
+                Result2.Text = button.Content.ToString();
             }
             else
-                Result.Text = Result.Text + button.Content;
+            {
+
+                if ((Result.Text == "0") || (isOperationPerformed))
+                {
+
+                    Result.Text = "";
+                    //Si on appuie sur la touche 0 on enlève alors que rien n'a été fait avant, on efface ce qu'il y a dans la textbox.
+                    //Idem si on vient d'effectuer l'opération.
+                }
+                isOperationPerformed = false;
+
+
+                if (button.Content == ".")
+                {
+                    if (!Result.Text.Contains("."))
+                    {
+                        Result.Text = Result.Text + button.Content;
+                        Result2.Text += Result.Text;
+                    }
+
+                }
+                else
+                {
+                    Result.Text += button.Content;
+                    Result2.Text += button.Content;
+                }
+            }
         }
 
 
-        private void Operator_click(object sender, RoutedEventArgs e){
+        //Use for      - + * /
+        private void Operator_click(object sender, RoutedEventArgs e)
+        {
             Button button = (Button)sender;
 
-            if (resultValue != 0)
-            {
-                //Enter.PerformClick();
-                operationPerformed = (String) button.Content;
-                //labelCurrentOperation.Text = resultValue + " " + operationPerformed;
-                isOperationPerformed = true;
-            }
-            else
-            {
 
-                operationPerformed = (String) button.Content;
+            if (isEqualPressed)
+            {
+                operationPerformed = (String)button.Content;
+                Result2.Text = Result.Text + " " + operationPerformed.ToString() + " ";
                 resultValue = Double.Parse(Result.Text);
-                //labelCurrentOperation.Text = resultValue + " " + operationPerformed;
                 isOperationPerformed = true;
-            }
 
+                //Result.Text = button.Content.ToString();
+                //Result2.Text = button.Content.ToString();
+                isEqualPressed = false;
+            }
+            else
+            {
+
+                if (resultValue != 0)
+                {
+
+                    operationPerformed = (String)button.Content;
+                    Result2.Text += " " + operationPerformed.ToString() + " ";
+                    isOperationPerformed = true;
+                    //labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                }
+                else
+                {
+
+                    operationPerformed = (String)button.Content;
+                    resultValue = Double.Parse(Result.Text);
+                    Result2.Text += " " + operationPerformed.ToString() + " ";
+                    isOperationPerformed = true;
+                    //labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                }
+            }
         }
 
-        private void Enter_click(object sender, RoutedEventArgs e){
+        //Use for =
+        private void Enter_click(object sender, RoutedEventArgs e)
+        {
             switch (operationPerformed)
             {
                 case "+":
                     Result.Text = (resultValue + Double.Parse(Result.Text)).ToString();
+                    isEqualPressed = true;
                     break;
                 case "-":
                     Result.Text = (resultValue - Double.Parse(Result.Text)).ToString();
+                    isEqualPressed = true;
                     break;
                 case "*":
                     Result.Text = (resultValue * Double.Parse(Result.Text)).ToString();
+                    isEqualPressed = true;
                     break;
                 case "/":
                     Result.Text = (resultValue / Double.Parse(Result.Text)).ToString();
+                    isEqualPressed = true;
                     break;
                 default:
                     break;
             }
-            //resultValue = Double.Parse(Result.Text);
-            //labelCurrentOperation.Text = "";
+
+            //We set value to the normal ones
+            resultValue = 0;
+            operationPerformed = "";
+            isOperationPerformed = false;
+
         }
 
+
+        private void ResetEverything(object sender, RoutedEventArgs e)
+        {
+            resultValue = 0;
+            operationPerformed = "";
+            isOperationPerformed = false;
+            isEqualPressed = false;
+            Result.Text = "0";
+            Result2.Text = "";
+        }
+
+
     }
-
-
 }
-
